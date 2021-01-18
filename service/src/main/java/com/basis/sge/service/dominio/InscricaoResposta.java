1eb44d0044e786b3bd3573cdbb38038b13ae9798
JoaoDiniz409 <joao.diniz@maisunifacisa.com.br>
2021-01-18 17:56:36 -0300

    pergunta e perguntaEvento

--- service/src/main/java/com/basis/sge/service/dominio/InscricaoResposta.java
+++ service/src/main/java/com/basis/sge/service/dominio/InscricaoResposta.java
@@ -4,15 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.Table;
 import java.io.Serializable;
 
@@ -23,30 +20,29 @@ import java.io.Serializable;
 @Setter
 public class InscricaoResposta implements Serializable {
 
-    @Id
+    @EmbeddedId
+    private IdInscricaoResposta id;
 
-    private Integer id;
+    @Column (name = "resposta")
+    private String resposta;
 
-
-    @Id
-
+    @MapsId("idInscricao")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_inscricao")
     private Inscricao inscricao;
 
-
+    @MapsId("idEvento")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_evento")
     private Evento evento;
 
-
+    @MapsId("idPergunta")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_pergunta")
     private Pergunta pergunta;
 
 
-    @Column (name = "resposta")
-    private String resposta;
+
 
 
 
