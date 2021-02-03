2a2cbf37aa3ca4abc8e91738c9c62efab0ff5e15
tsm77 <tiagosantiagotsm77@gmail.com>
2021-02-03 17:41:01 -0300

    Administrador escondendo

--- service/frontend/src/app/app.component.ts
+++ service/frontend/src/app/app.component.ts
@@ -45,14 +45,22 @@ export class AppComponent implements AfterViewInit, OnDestroy, OnInit {
 
     rippleMouseDownListener: EventListenerOrEventListenerObject;
 
-    constructor(public renderer2: Renderer2, public zone: NgZone, public menuService: MenusService) { }
+    constructor(
+         public renderer2: Renderer2, public zone: NgZone, public menuService: MenusService) { }
 
     ngOnInit() {
         this.zone.runOutsideAngular(() => { this.bindRipple(); });
 
+        /*this.loginService.verificarLogoni().subscribe(usuario => {
+            if (usuario.admin) {
+
+            } else {
+
+            }
+        });*/
         this.menuService.itens = [
             { label: 'Evento', icon: 'event_note', routerLink: ['/eventos'] },
-            { label: 'Usuario', icon: 'account_circle', routerLink: ['/usuarios'] },
+            { label: 'Administrador', icon: 'account_circle', routerLink: ['/usuarios'] },
             { label: 'Perfil', icon: 'dashboard', routerLink: ['/minha-conta'] },
         ];
 
